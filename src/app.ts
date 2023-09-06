781caa9356d5a56b27fdcc11406a05b066b1d675
Tarık <utariktemel@gmail.com>
2023-09-07 01:32:01 +0200

    qrcode-terminal and wpweb.js test

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
-let fun = (text: string): string => {
-    return "Hello World " + text;
-}
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
 
-console.log(fun("Tarık"));
+const client = new Client();
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', (msg) => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
