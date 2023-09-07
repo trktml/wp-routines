9b39d51af292e7cac38e47396172191fe12f44d8
Tarık <utariktemel@gmail.com>
2023-09-07 02:24:19 +0200

    save LocalAuth

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,36 @@
 import qrcode from 'qrcode-terminal';
-import { Client } from "whatsapp-web.js";
+import wpweb from "whatsapp-web.js";
+
+const { Client, LocalAuth } = wpweb;
 
 const client = new Client({
     puppeteer: {
         args: ['--no-sandbox'],
-    }
+    },
+    authStrategy: new LocalAuth(),
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
 });
+
+client.on("authenticated", () => {
+    console.log("Client is authenticated!");
+})
 
 client.on('qr', (qr) => {
     // Generate and scan this code with your phone
     qrcode.generate(qr, { small: true });
 });
+
+client.on("disconnected", () => {
+    console.log("Client is disconnected!");
+});
+
+client.on("loading_screen", () => {
+    console.log("Loading screen!");
+});
+
+client.on("auth_failure", () => {
+    console.log("Authentication failed!");
+})
 
 client.on('ready', () => {
     console.log('Client is ready!');
