58acdb61a8367bb130d18a9fde562edf25e47a9d
Tarık <utariktemel@gmail.com>
2023-10-05 19:02:56 +0200

    sql location

--- web/pages/api/crons/add.ts
+++ web/pages/api/crons/add.ts
@@ -10,7 +10,7 @@ export default function handler(
     res: NextApiResponse<ResponseData>
 ) {
     // sqlite get all crons from crons table column names are name and date
-    const db = new sqlite3.Database('pages/api/crons/crons.sqlite', (err) => {
+    const db = new sqlite3.Database('crons.sqlite', (err) => {
         if (err) {
             console.error(err.message);
         }
